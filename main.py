43116d1d6f9fbb8fc4539cfc9e2bed5742a6c1ea
Doğukan Ürker <dogukanurker@icloud.com>
2023-09-19 23:30:33 +0300

    ✨ "/about" & "/whatisablackhole" added

--- main.py
+++ main.py
@@ -31,6 +31,16 @@ def blackHoles():
     return data()
 
 
+@app.get("/about")
+def about():
+    return "This list of black holes (and stars considered probable candidates) is organized by mass (including black holes of undetermined mass); some items in this API are galaxies or star clusters that are believed to be organized around a black hole."
+
+
+@app.get("/whatisablackhole")
+def whatIsBlackHole():
+    return "A black hole is a region of spacetime where gravity is so strong that nothing, including light or other electromagnetic waves, has enough energy to escape it. The theory of general relativity predicts that a sufficiently compact mass can deform spacetime to form a black hole. The boundary of no escape is called the event horizon. Although it has a great effect on the fate and circumstances of an object crossing it, it has no locally detectable features according to general relativity. In many ways, a black hole acts like an ideal black body, as it reflects no light. Moreover, quantum field theory in curved spacetime predicts that event horizons emit Hawking radiation, with the same spectrum as a black body of a temperature inversely proportional to its mass. This temperature is of the order of billionths of a kelvin for stellar black holes, making it essentially impossible to observe directly."
+
+
 @app.get("/data/{id}")
 def getByID(id):
     for i in data():
